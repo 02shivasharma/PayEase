528fa402d24be2b73bac5714acb566fa3d1e667a
Shiva Sharma <shiva707878@gmail.com>
2024-08-30 19:35:19 +0530

    updated some components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,24 @@ import {SignUp} from "./pages/SignUp"
 import {SignIn} from "./pages/SignIn"
 import {Dashboard} from "./pages/Dashboard"
 import {Sendmoney} from "./pages/Sendmoney"
+import {AppBar} from "./component/AppBar"
+import {Balance} from "./component/Balance"
+import {Users} from "./component/Users"
+import {SendMoney} from "./component/SendMoney"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-     <BrowserRouter>
-       <Routes>
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/signIn' element={<SignIn />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/send' element={<Sendmoney />} />
-       </Routes>
-     </BrowserRouter>
+    //  <BrowserRouter>
+    //    <Routes>
+    //     <Route path='/signup' element={<SignUp />} />
+    //     <Route path='/signIn' element={<SignIn />} />
+    //     <Route path='/dashboard' element={<Dashboard />} />
+    //     <Route path='/send' element={<Sendmoney />} />
+    //    </Routes>
+    //  </BrowserRouter>
+    //  <AppBar/>
+      <SendMoney />
   )
 }
 
